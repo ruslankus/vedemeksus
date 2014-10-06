b742f85010a5d2543a07d3112d0ab33f2f15cad7
Ruslan Kiricenko <ruslankus@yahoo.com>
2014-10-07 00:35:12 +0300

    added responsive courusell

---
+++ js/product.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+     $("#carousel").owlCarousel({
+		items : 3,
+		itemsDesktop : [1200,3],
+		itemsDesktopSmall : [992,2],
+		itemsTablet: [768,1],
+		itemsMobile : false, 
+		
+		navigation : true 
+	});
+	
+
+	
+});
